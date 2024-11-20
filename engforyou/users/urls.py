cbae8d719cab2341f6eea271f7e7922e0ccc5b11
a27lis <anastasiya.liseykina@gmail.com>
2024-11-20 15:35:45 +0400

    profile

--- engforyou/users/urls.py
+++ engforyou/users/urls.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.views import PasswordChangeDoneView
+
 from django.urls import path
 from . import views
 
@@ -6,6 +8,10 @@ app_name = "users"
 urlpatterns = [
     path('login/', views.LoginUser.as_view(), name='login'),
     path('register/', views.RegisterUser.as_view(), name='register'),
-    path('logout/', views.logout_user, name='logout')
+    path('logout/', views.logout_user, name='logout'),
+    path('profile/', views.ProfileUser.as_view(), name='profile'),
+    path('password-change/', views.UserPasswordChange.as_view(), name="password_change"),
+    path('password-change/done/', PasswordChangeDoneView.as_view(template_name="users/password_change_done.html"), name="password_change_done"),
+
     
 ]
--- engforyou/users/views.py
+++ engforyou/users/views.py
@@ -1,13 +1,11 @@
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.http import HttpResponse, HttpResponseRedirect
-from django.views.generic import CreateView
+from django.contrib.auth import logout, get_user_model
+from django.contrib.auth.views import LoginView, PasswordChangeView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
+from django.views.generic import CreateView, UpdateView
 from django.urls import reverse, reverse_lazy
 
-from .forms import LoginUserForm, RegisterUserForm
+from .forms import LoginUserForm, RegisterUserForm, ProfileUserForm, UserPasswordChangeForm
 
 
 class LoginUser(LoginView):
@@ -22,8 +20,25 @@ class RegisterUser(CreateView):
         if path:
             return f"{reverse_lazy('users:login')}?next={path}"
         return reverse_lazy('users:login')
-        
+    
+class UserPasswordChange(PasswordChangeView):
+    form_class = UserPasswordChangeForm
+    success_url = reverse_lazy("users:password_change_done")
+    template_name = "users/password_change_form.html"
+
 
 def logout_user(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+
+class ProfileUser(LoginRequiredMixin, UpdateView):
+    model = get_user_model()
+    form_class = ProfileUserForm
+    template_name = 'users/profile.html'
+
+    def get_success_url(self):
+        return reverse_lazy('users:profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user
