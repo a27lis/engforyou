c38e5e130438189d89c78786d4cfc1d756f456ed
a27lis <anastasiya.liseykina@gmail.com>
2024-11-21 13:16:09 +0400

    abstract user

--- engforyou/users/forms.py
+++ engforyou/users/forms.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, PasswordChangeForm
@@ -36,12 +38,14 @@ class ProfileUserForm(forms.ModelForm):
     username = forms.CharField(disabled=True, label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
     email = forms.CharField(disabled=True, label='E-mail', widget=forms.TextInput(attrs={'class': 'form-input'}))
 
+
     class Meta:
         model = get_user_model()
         fields = ['username', 'email', 'first_name', 'last_name']
         labels = {
             'first_name': 'Имя',
             'last_name': 'Фамилия',
+            
         }
         widgets = {
             'first_name': forms.TextInput(attrs={'class': 'form-input'}),
--- engforyou/users/models.py
+++ engforyou/users/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    date_birth = models.DateTimeField(blank=True, null=True, verbose_name="Дата рождения")
+
